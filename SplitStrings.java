20287abfdbd769b62cef8aac2129b4b52cceb974
KyleDTan <2212063@slu.edu.ph>
2022-08-25 16:14:37 +0800

    Split strings

--- SplitStrings.java
+++ SplitStrings.java
@@ -6,7 +6,7 @@ public class SplitStrings {
         int arrayLength = s.length() / 2;
         String[] pairedCharas = new String[arrayLength];
         int counter = 0;
-        for(int x = 0; x < pairedCharas.length; x++){
+        for(int x = 0; x < s.length(); x++){
             StringBuilder string = new StringBuilder();
             string.append(s.charAt(x));
             string.append(s.charAt(x+1));
@@ -16,12 +16,11 @@ public class SplitStrings {
             x++;
         }
 
-        //Write your code here
         return pairedCharas;
     }
 
     public static void main(String[] args) {
-        String[] array = solution("abcdef");
+        String[] array = solution("abcdefg");
         for(int x = 0; x < array.length; x++){
             System.out.print(array[x] + " ");
         }
