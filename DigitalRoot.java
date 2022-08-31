48fd52898013d302d2f86a385567e8c853fdfe6b
KyleDTan <2212063@slu.edu.ph>
2022-08-31 14:48:30 +0800

    Digital root Update

--- DigitalRoot.java
+++ DigitalRoot.java
@@ -12,13 +12,6 @@ public class DigitalRoot {
         return root;
     }
 
-    /**
-     * Best Practice
-     * public static int digital_root(int n) {
-     *     return (n != 0 && n%9 == 0) ? 9 : n % 9;
-     * }
-     */
-
     public static void main(String[] args) {
         System.out.println(digital_root(16));
         System.out.println(digital_root(456));
